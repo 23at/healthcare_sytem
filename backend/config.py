7cef957780e8d9fc63dff182121657245a0ecf45
23at <asmita2023@gmail.com>
2025-09-26 17:27:34 -0500

    switched around the order of CORS function

--- backend/config.py
+++ backend/config.py
@@ -5,9 +5,13 @@ from flask_cors import CORS
 app=Flask(__name__)
 app.secret_key = "your-unique-secret-key"  # 🔐 Replace with a secure, random value
 
-CORS(app,origins=["http://localhost:3000", "https://healthcare-sytem-frontend.onrender.com"], supports_credentials=True)
+
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///healthcare_database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
+
 
 db=SQLAlchemy(app)
+CORS(app,origins=["http://localhost:3000", "https://healthcare-sytem-frontend.onrender.com"], supports_credentials=True)

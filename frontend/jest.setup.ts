ce5f208bd2244bf6c199e1a609e9c10726896542
siandim <es.dim13@gmail.com>
2025-09-28 20:19:58 -0500

    added jest.setup.ts

--- frontend/jest.setup.ts
+++ frontend/jest.setup.ts
@@ -1,9 +1,18 @@
 import "@testing-library/jest-dom";
 
 jest.mock("next/navigation", () => ({
-    useRouter: () => ({
-      push: jest.fn(),
-      replace: jest.fn(),
-      prefetch: jest.fn(),
-    }),
-  }));
+  useRouter: () => ({
+    push: jest.fn(),
+    replace: jest.fn(),
+    prefetch: jest.fn(),
+  }),
+}));
+
+// Suppress act warnings in CI
+const originalError = console.error;
+console.error = (...args) => {
+  if (args[0].includes("React.act is not a function")) {
+    return;
+  }
+  originalError(...args);
+};
